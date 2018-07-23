47fcc38388399ece78d467c1667562dcb5e193b5
cmmmli <thekomori1113@gmail.com>
2018-07-23 22:24:03 +0900

    [modify] apply room.avatar

--- chat/room.go
+++ chat/room.go
@@ -20,16 +20,19 @@ type room struct {
 	clients map[*client]bool
 	// tracerはチャットルーム上で行われた操作のログを受け取る
 	tracer trace.Tracer
+	// avatarはアバターの情報を取得する
+	avatar Avatar
 }
 
 // newRoomはすぐに利用できるチャットルームを生成して返します。
-func newRoom() *room {
+func newRoom(avatar Avatar) *room {
 	return &room{
 		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
 		tracer:  trace.Off(),
+		avatar:  avatar,
 	}
 }
 
